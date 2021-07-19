4b041b0b36b3622970d20bd5be427a1e96708e4c
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-07-19 20:52:06 +0530

    Update sample_config.py

--- YoneRobot/sample_config.py
+++ YoneRobot/sample_config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1883752632  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Its_JassManak"
-    SUPPORT_CHAT = "Kakashi_Support"  # Your own group for support, do not add the @
+    OWNER_ID = "1619759800  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "DARKAMAN"
+    SUPPORT_CHAT = "DARKXV2SUPPORT"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001381913288
+        -1001459177818
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001282131997
+        -1001459177818
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
