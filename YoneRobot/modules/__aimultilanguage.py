2a6bcf624060e2cbba59d2a8e6048602c1c23eaf
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-07-19 19:46:13 +0530

    Update __aimultilanguage.py

--- YoneRobot/modules/__aimultilanguage.py
+++ YoneRobot/modules/__aimultilanguage.py
@@ -59,17 +59,17 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Kakashi AI Already Activated In This Chat")
+            await lel.edit("𓆩ɖǟʀӄӼɮօȶ𓆪 AI Already Activated In This Chat")
             return
         await lel.edit(
-            f"Kakashi AI Successfully Added For Users In The Chat {message.chat.id}"
+            f"𓆩ɖǟʀӄӼɮօȶ𓆪 AI Successfully Added For Users In The Chat {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.editDARKX AI Was Not Activated In This Chat")
+            await lel.edit("𓆩ɖǟʀӄӼɮօȶ𓆪 AI Was Not Activated In This Chat")
             return
         await lel.edit(
             f"𓆩ɖǟʀӄӼɮօȶ𓆪 AI Successfully Deactivated For Users In The Chat {message.chat.id}"
