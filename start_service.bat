3c5250a9446c4ba1cc199cf87b8c39366283f0b1
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-07-19 21:02:04 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop Kakashi Robot
-net start Kakashi Robot
+net stop DARKXV2BOT
+net start DARKXV2BOT
