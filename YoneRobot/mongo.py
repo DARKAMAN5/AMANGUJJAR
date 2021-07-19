bbfa566835245f446ec46742b6a96d1f1f958fdd
DARKAMAN5 <86722987+DARKAMAN5@users.noreply.github.com>
2021-07-19 21:27:41 +0530

    Update mongo.py

--- YoneRobot/mongo.py
+++ YoneRobot/mongo.py
@@ -18,7 +18,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["DARKX V2 RoBot"]
+db = client["DARKXV2 BOT"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
